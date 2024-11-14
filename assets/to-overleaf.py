689599e65fc58b10a10d0bc0dd0465bec79ff7fa
Jasraj Singh <jasraj001@ntu.edu.sg>
2024-11-14 19:55:27 +0530

    cleaned assets directory

--- assets/to-overleaf.py
+++ assets/to-overleaf.py
@@ -1,39 +1,9 @@
 import os
 import shutil
-
-##########
-
-# assets_dir = './sensitivity/model-store'
-
-# for dataset in os.listdir(assets_dir):
-#     dataset_dir = f'{assets_dir}/{dataset}'
-#     for fn in os.listdir(dataset_dir):
-#         fn = f'{dataset_dir}/{fn}'
-#         if os.path.isfile(fn):
-#             os.remove(fn)
-
-##########
-
-# assets_dir = './sensitivity/model-store'
-
-# for dataset in os.listdir(assets_dir):
-#     dataset_dir = f'{assets_dir}/{dataset}'
-#     for which in os.listdir(dataset_dir):
-#         which_dir = f'{dataset_dir}/{which}'
-#         for metric in os.listdir(which_dir):
-#             src = f'{which_dir}/{metric}'
-#             dst = f"{which_dir}/{'-'.join(metric.lower().split())}"
-#             if src != dst:
-#                 if ' ' in src and os.path.isdir(dst):
-#                     shutil.rmtree(dst)
-#                 os.rename(src, dst)
-
-##########
-
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dirs', nargs='+', type=str, required=True)
+parser.add_argument('--dirs', nargs='+', type=str)
 args = parser.parse_args()
 
 for dir in args.dirs:
@@ -63,28 +33,3 @@ for dir in args.dirs:
             os.remove(dst_name)
         os.makedirs(os.path.dirname(dst_name), exist_ok=True)
         shutil.copy2(src_name, dst_name)
-
-##########
-
-# dirs = os.listdir('.')
-
-# for dir in dirs:
-
-#     if dir in ('__pycache__', 'overleaf'):
-#         continue
-
-#     src_names = [
-#         f'{dirpath}/{d}'.replace('\\', '/')
-#         for (dirpath, dirnames, _) in os.walk(dir) 
-#         for d in dirnames
-#     ]
-
-#     dst_names = [
-#         d.replace('accuracy', 'Accuracy').replace('f1-score', 'F1 Score').replace('cross-entropy-loss', 'Cross Entropy Loss')
-#         for d in src_names
-#     ]
-
-#     for src_name, dst_name in zip(src_names, dst_names):
-        
-#         if src_name != dst_name:
-#             os.rename(src_name, dst_name)

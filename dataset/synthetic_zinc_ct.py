7b5b9208cb983849656704c32010a30fbb55149f
ignasa007 <jasraj001@ntu.edu.sg>
2025-01-23 19:23:33 +0000

    cleaned

--- dataset/synthetic_zinc_ct.py
+++ dataset/synthetic_zinc_ct.py
@@ -20,7 +20,7 @@ class Transform_CT(Transform_SD):
 
     def save_node_pairs(self, root, alpha, split):
 
-        fn = f'{root}/node-pairs-sd/alpha={alpha}/{split}.pkl'
+        fn = f'{root}/node-pairs-ct/alpha={alpha}/{split}.pkl'
         if os.path.isfile(fn): 
             with open(fn, 'rb') as f:
                 node_pairs = pickle.load(f)
@@ -34,13 +34,10 @@ class Transform_CT(Transform_SD):
         # For each molecule, sample a node pair separated by `distance` hops
         for datum in tqdm(dataset):
             commute_times = compute_commute_times(datum.edge_index)
-            quantile = torch.quantile(commute_times.flatten(), alpha, interpolation='nearest')
-            choices = torch.where(commute_times == quantile)                # Tuple[row indices, column indices]
-            try:
-                sample = random.randint(0, choices[0].size(0)-1)            # Int in range [0, num_mathces-1]
-                node_pair = list(map(lambda x: x[sample].item(), choices))  # List[row, column]
-            except ValueError:
-                node_pair = None                                            # No pair separated by `distance` hops
+            quantile = torch.quantile(commute_times.flatten(), q=alpha, interpolation='nearest')
+            choices = torch.where(commute_times == quantile)            # Tuple[row indices, column indices]
+            sample = random.randint(0, choices[0].size(0)-1)            # Int in range [0, num_mathces-1]
+            node_pair = list(map(lambda x: x[sample].item(), choices))  # List[row, column]
             node_pairs.append(node_pair)
 
         os.makedirs(os.path.dirname(fn), exist_ok=True)
@@ -67,9 +64,7 @@ class SyntheticZINC_CT(Inductive):
                 random.shuffle(dataset)
                 dataset = dataset[:size]
             # Create node-level features, and graph-level labels
-            data_list = [transform(index, datum) for index, datum in dataset]
-            # Filter out molecules with no two nodes separated by `distance` hops
-            data_list = [datum.to(device) for datum in data_list if datum is not None]
+            data_list = [transform(index, datum).to(device) for index, datum in dataset]
             datasets.append(CustomDataset(data_list))
         train, val, test = datasets
         

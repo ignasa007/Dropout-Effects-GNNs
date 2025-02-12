4ed1dae7689ce6870e9c8a0c47335a595b54ee3a
ignasa007 <jasraj001@ntu.edu.sg>
2025-02-12 07:41:54 +0000

    utility function to analyze a run

---
+++ plots/metrics/vs_epochs.py
@@ -0,0 +1,23 @@
+import argparse
+import matplotlib.pyplot as plt
+from utils.parse_logs import parse_metrics
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', type=str, required=True)
+parser.add_argument('--metric', type=str)
+args = parser.parse_args()
+
+train, val, test = parse_metrics(args.path)
+if args.metric is None:
+    if 'Mean Absolute Error' in train:
+        args.metric = 'Mean Absolute Error'
+    else:
+        args.metric = 'Cross Entropy Loss'
+
+plt.plot(train[args.metric], label='train')
+plt.plot(val[args.metric], label='val')
+plt.plot(test[args.metric], label='test')
+
+plt.legend()
+plt.grid()
+plt.savefig(f'assets/{args.path.replace('/', '-')}.png')

c17cb92ed03770f271d3d9f32e125acf03595986
ignasa007 <jasraj001@ntu.edu.sg>
2025-02-12 07:44:23 +0000

    better samples counting

--- experiments/black.sh
+++ experiments/black.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-total_samples=20
+total_samples=30
 dataset=${1}
 gnn=${2}
 device_index=${3}
@@ -17,16 +17,16 @@ learning_rate=1e-3
 weight_decay=0
 n_epochs=300
 
-
 for dropout in "${dropouts[@]}"; do
     for drop_p in $( [[ "$dropout" == "NoDrop" ]] && echo "0.0" || echo "${drop_ps[@]}" ); do
-        num_samples=$(find results/${dropout}/${dataset}/${gnn}/L=${depth}/P=${drop_p} -mindepth 1 -type d 2>/dev/null | wc -l)
+        config_dir="./results/${dropout}/${dataset}/${gnn}/L=${depth}/P=${drop_p}"
+        num_samples=$(find ${config_dir} -mindepth 1 -type d 2>/dev/null | wc -l)
         while [ ${num_samples} -lt ${total_samples} ]; do
             python -B main.py \
                 --dataset ${dataset} \
                 --gnn ${gnn} \
                 --gnn_layer_sizes ${hidden_size}*${depth} \
-                $( [[ "${gnn}" == "GAT" ]] && echo "--attention_heads ${attention_heads}" ) \
+                $( [[ "${gnn}" == "GAT" ]] && echo --attention_heads ${attention_heads} ) \
                 --bias ${bias} \
                 --pooler ${pooler} \
                 --dropout ${dropout} \
@@ -35,6 +35,7 @@ for dropout in "${dropouts[@]}"; do
                 --weight_decay ${weight_decay} \
                 --n_epochs ${n_epochs} \
                 --device_index ${device_index} \
+                --exp_dir ${config_dir}/$(date "+%Y-%m-%d-%H-%M-%S") \
             && num_samples=$((${num_samples}+1));
         done
     done

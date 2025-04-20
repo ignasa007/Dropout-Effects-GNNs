790f5e4d1000874cb0ac9d8c7b44f89fafab29e4
ignasa007 <jasraj001@ntu.edu.sg>
2025-04-20 14:38:36 +0100

    filtering ouout some DropSens configs

--- plots/metrics/drop_sens.py
+++ plots/metrics/drop_sens.py
@@ -11,12 +11,13 @@ from utils.parse_logs import parse_metrics
 datasets = ('Cora', 'CiteSeer', 'PubMed', 'Chameleon', 'Squirrel', 'TwitchDE', 'Actor') + \
     ('Mutag', 'Proteins', 'Enzymes', 'Reddit', 'IMDb', 'Collab')
 gnn = 'GCN'
-dropout = 'DropSens'
+baseline = 'DropEdge'
+dropouts = ('DropSens',)
 
 metric = 'Accuracy'
-drop_ps = np.round(np.arange(0.1, 1, 0.1), decimals=1)
+drop_ps = np.round(np.arange(0.0, 1, 0.1), decimals=1)
 info_loss_ratios = (0.5, 0.8, 0.9, 0.95)
-exp_dir = './results/{dropout}/{dataset}/{gnn}/L=4/P={drop_p}/C={info_loss_ratio}'
+exp_dir = './results/{dataset}/{gnn}/L=4/{dropout}/P={drop_p}/C={info_loss_ratio}'
 
 
 def get_samples(dataset, gnn, dropout, drop_p, info_loss_ratio=None):
@@ -37,9 +38,6 @@ def get_samples(dataset, gnn, dropout, drop_p, info_loss_ratio=None):
         sample = test[metric][np.argmax(val[metric])]
         samples.append(sample)
 
-    if len(samples) < 20:
-        print(dataset, gnn, drop_p, info_loss_ratio)
-
     return samples
 
 def get_best(dataset, gnn, dropout):
@@ -48,6 +46,8 @@ def get_best(dataset, gnn, dropout):
 
     for drop_p in drop_ps:
         for info_loss_ratio in (info_loss_ratios if dropout == 'DropSens' else (None,)):
+            if (drop_p, info_loss_ratio) in ((0.2, 0.5), (0.3, 0.5), (0.5, 0.5), (0.2, 0.8)):
+                continue
             samples = get_samples(dataset, gnn, dropout, drop_p, info_loss_ratio)
             # Use at least 10 samples and at most 20 samples for computing the best config
             mean = np.mean(samples[:20]) if len(samples) >= 10 else np.nan
@@ -57,30 +57,35 @@ def get_best(dataset, gnn, dropout):
     # Return all samples (more than 20 for the best config)
     return best_samples
 
-delta_error_rates = list()
-
-for dataset in tqdm(datasets):
-    best_de_samples = get_best(dataset, gnn, 'DropEdge')
-    if not best_de_samples:
-        continue
-    best_de_mean, best_de_std = np.mean(best_de_samples), np.std(best_de_samples, ddof=1)
-    best_ds_samples = get_best(dataset, gnn, 'DropSens')
-    if not best_ds_samples:
-        continue
-    best_ds_mean, best_ds_std = np.mean(best_ds_samples), np.std(best_ds_samples, ddof=1)
-    delta_error_rates.append(100*((1-best_de_mean)-(1-best_ds_mean))/(1-best_de_mean))
 
 fig, ax = plt.subplots(1, 1, figsize=(11.2, 4.8))
 xs_de = np.arange(len(datasets))
-ax.bar(x=xs_de, height=delta_error_rates)
+width = 0.8/len(dropouts)
+displacements = np.arange(-(len(dropouts)-1), len(dropouts), 2) * (width/2)
 
-ax.set_xticks(xs_de, datasets, rotation=45, fontsize=15)
-yticks = np.arange(-2.5, 22.5, 2.5)
-ax.set_yticks(yticks, yticks, fontsize=15)
+baseline_samples = dict()
+for dataset in tqdm(datasets):
+    best_samples = get_best(dataset, gnn, baseline)
+    baseline_samples[dataset] = best_samples
+
+for dropout, displacement in zip(dropouts, displacements):
+    heights = list()
+    for dataset in tqdm(datasets):
+        best_samples = get_best(dataset, gnn, dropout)
+        if not best_samples:
+            continue
+        best_mean, best_std = np.mean(best_samples[:50]), np.std(best_samples[:50], ddof=1)
+        baseline_mean = np.mean(baseline_samples[dataset][:50])
+        heights.append(100*(best_mean-baseline_mean)/(1-baseline_mean))
+    ax.bar(x=xs_de+displacement, height=heights, width=width, label=dropout)
+
+ax.set_xticks(xs_de, datasets, rotation=30, fontsize=15)
+# yticks = np.arange(-2.5, 22.5, 2.5)
+# ax.set_yticks(yticks, yticks, fontsize=15)
 ax.set_ylabel('Relative Error Change (%)', fontsize=18)
 ax.grid()
 
 fig.tight_layout()
-fn = './assets/DropSens/errors-diff.png'
+fn = './assets/DropSens/tmp.png'
 os.makedirs(os.path.dirname(fn), exist_ok=True)
 plt.savefig(fn)
--- plots/metrics/philia.py
+++ plots/metrics/philia.py
@@ -2,85 +2,82 @@ import argparse
 import os
 import warnings; warnings.filterwarnings('ignore')
 from collections import defaultdict
+from tqdm import tqdm
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-from utils.parse_logs import parse_metrics, parse_configs
+from utils.parse_logs import parse_metrics
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--gnn', type=str, default='GCN', choices=['GCN', 'GAT'])
 parser.add_argument('--dropout', type=str, default='DropEdge')
+parser.add_argument('--train', action='store_true')
 args = parser.parse_args()
+
+metric = 'Accuracy'
+if args.train:
+    get_value = lambda train, val, test: np.max(train[metric])
+    ylabel = 'Train'
+else:
+    get_value = lambda train, val, test: np.max(test[metric][np.argmax(val[metric])])
+    ylabel = 'Test'
 
 homo_data = ('Cora', 'CiteSeer', 'PubMed')
 hetero_data = ('Chameleon', 'Squirrel', 'TwitchDE')
-homo_cutoffs = (0.3021, 0.2107)
-hetero_cutoffs = (0.2288, 0.1203, 0.6045)
-
-metric = 'Accuracy'
 depths = range(2, 9, 2)
-ps = np.round(np.arange(0.0, 1, 0.1), decimals=1)
-
+ps = np.round(np.arange(0.1, 1, 0.1), decimals=1)
 ncol = np.ceil(len(depths)/1)
 
-for fn, datasets, cutoffs in zip(('homophilic', 'heterophilic'), (homo_data, hetero_data), (homo_cutoffs, hetero_cutoffs)):
+for datasets, fn in zip((homo_data, hetero_data), ('homophilic', 'heterophilic')):
 
     fig, axs = plt.subplots(1, len(datasets), figsize=(6.4*len(datasets), 4.8))
     if not hasattr(axs, '__len__'): axs = (axs,)
 
-    axs[0].set_ylabel(f'Test {metric}', fontsize=20)
+    axs[0].set_ylabel(f'{ylabel} {metric}', fontsize=20)
 
-    for dataset, cutoff, ax in zip(datasets, cutoffs, axs):
+    for dataset, ax in tqdm(zip(datasets, axs)):
 
-        results_dir = f'./results/{args.dropout}/{dataset}'
-        exp_dir = results_dir + '/{args.gnn}/L={depth}/P={p}'
+        assets_dir = f"./assets/philia/{args.gnn}/{args.dropout}/{ylabel}"
+        exp_dir = f'./results/{dataset}/{args.gnn}/' + 'L={depth}/' + f'{args.dropout}/' + 'P={p}'
 
         ### RETRIEVE METRICS ###
 
-        test_metrics = defaultdict(list)
+        metrics = defaultdict(list)
 
         for depth in depths:
-            for p in ps:
-                exp_dir_format = exp_dir.format(gnn=args.gnn, depth=depth, p=p)
+            for drop_p in ps:
+                exp_dir_format = exp_dir.format(depth=depth, p=drop_p)
                 if not os.path.exists(exp_dir_format):
                     continue
                 for sample_dir in os.listdir(exp_dir_format):
-                    config = parse_configs(f'{exp_dir_format}/{sample_dir}/logs')
-                    if not all([x == 64 for x in eval(config['gnn_layer_sizes'])]):
-                        print(f'{exp_dir_format}/{sample_dir}')
                     train, val, test = parse_metrics(f'{exp_dir_format}/{sample_dir}/logs')
                     if len(train[metric]) != 300:
                         continue
-                    value = np.max(test[metric][np.argmax(val[metric])])
-                    if value > cutoff:
-                        test_metrics[(depth, p)].append(value)
-                    
-        for depth in depths:
-            for p in ps:
-                if len(test_metrics[(depth, p)]) != 5:
-                    print(dataset, args.gnn, depth, p, len(test_metrics[(depth, p)]))
+                    value = get_value(train, val, test)
+                    metrics[(depth, drop_p)].append(value)
 
         ### PLOT FOR TEST SET ###
 
-        test_metrics = {exp: (np.mean(samples[:5]), np.std(samples[:5])) for exp, samples in test_metrics.items()}
+        metrics = {exp: (np.mean(samples[:20]), np.std(samples[:20])) for exp, samples in metrics.items()}
 
         for depth in depths:
-            drop_ps, means, lower, upper = list(), list(), list(), list()
+            drop_ps, means, stds = list(), list(), list()
             for drop_p in ps:
-                if (depth, drop_p) in test_metrics:
-                    mean, std = test_metrics[(depth, drop_p)]
-                    drop_ps.append(drop_p); means.append(mean); lower.append(mean-std); upper.append(mean+std)
-            ax.plot(drop_ps, means, label=f'L = {depth}')
+                if (depth, drop_p) in metrics:
+                    mean, std = metrics[(depth, drop_p)]
+                    drop_ps.append(drop_p); means.append(mean); stds.append(std)
+            # ax.plot(drop_ps, means, label=f'L = {depth}')
+            ax.errorbar(drop_ps, means, yerr=stds, capsize=3, fmt='o--', ecolor='black', label=f'L = {depth}')
         ax.set_xlabel(fr'{args.dropout} Probability, $q$', fontsize=20)
         ax.set_title(dataset, fontsize=20)
         ax.grid()
 
-    if len(datasets) == 3:
+    if fn == 'heterophilic':
         handles, labels = ax.get_legend_handles_labels()
         fig.legend(handles, labels, loc='upper center', fontsize=18, ncol=ncol, bbox_to_anchor = (0.5, -0.02))
     fig.tight_layout()
-    fn = f'./assets/{args.dropout}/{fn}.png'
+    fn = f'./{assets_dir}/{fn}.png'
     os.makedirs(os.path.dirname(fn), exist_ok=True)
     plt.savefig(fn, bbox_inches='tight')
